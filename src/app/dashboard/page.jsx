20873e97be962f94651f2b60db0400b74f075398
DANdelion-0908 <pen22217@uvg.edu.gt>
2025-05-03 21:55:56 -0600

    Add shopping cart functionality to dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -18,6 +18,7 @@ export default function Dashboard ({ handleAuth }) {
     const [selectedDishes, setSelectedDishes] = useState([]);
     const [user, setUser] = useState({});
     const router = useRouter();
+    const [shoppingCart, setShoppingCart] = useState([]);
 
     useEffect(() => {
         const userData = localStorage.getItem("user");
@@ -53,6 +54,21 @@ export default function Dashboard ({ handleAuth }) {
         setIsRestaurant(false);
     }
 
+    const handleDish = (img, name, price, description) => {
+        const existingDish = shoppingCart.find(item => item.name === name);
+        if (existingDish) {
+          setShoppingCart(shoppingCart.map(item =>
+            item.name === name ? { ...item, quantity: item.quantity + 1 } : item
+          ));
+        } else {
+          setShoppingCart([...shoppingCart, { img, name, price, description, quantity: 1 }]);
+        }
+    }
+      
+    const removeDish = (indexToRemove) => {
+        setShoppingCart(shoppingCart.filter((_, index) => index !== indexToRemove));
+    }
+
     const handleUser = () => {
         router.push("/user");
     }
@@ -61,6 +77,78 @@ export default function Dashboard ({ handleAuth }) {
   return (
     <>
         <Header toHome={toHome}/>
+        
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <dialog id="shopping_cart" className="modal modal-bottom w-full">
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">Carrito de compras</h3>
+                <ul className="list bg-base-100 rounded-box shadow-md">
+                    { shoppingCart.map((item, index) => (                    
+                        <li className="list-row flex justify-between h-50" key={index}>
+                            <div className='flex flex-row gap-10 justify-center align-middle items-center'>
+                                <div><img className="avatar max-w-20 rounded-box" src={item.img}/></div>
+                                <div>
+                                    <div className='w-auto text-md'>{item.name}</div>
+                                    <div className="w-auto text-md uppercase font-semibold opacity-60">Q{item.price}</div>
+                                </div>
+                                <p className="list-col-wrap text-md">{item.description}</p>
+                            </div>
+                            <div className='flex flex-row gap-2 justify-center align-middle items-center'>                                
+                            <button onClick={() => {
+                                setShoppingCart(cart =>
+                                    cart.map((item, i) =>
+                                    i === index && item.quantity > 1
+                                        ? { ...item, quantity: item.quantity - 1 }
+                                        : item
+                                    )
+                                );
+                                }} className="btn btn-square btn-ghost">–</button>
+
+                                <p className='text-center w-5'>{item.quantity}</p>
+
+                                <button onClick={() => {
+                                setShoppingCart(cart =>
+                                    cart.map((item, i) =>
+                                    i === index
+                                        ? { ...item, quantity: item.quantity + 1 }
+                                        : item
+                                    )
+                                );
+                                }} className="btn btn-square btn-ghost">+</button>
+
+                                <button
+                                onClick={() => removeDish(index)}
+                                className="btn btn-square self-start btn-error text-white"
+                                aria-label="Eliminar platillo"
+                                >
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
+                                    <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-4.5l-1-1z"/>
+                                </svg>
+                                </button>
+
+                            </div>
+                        </li>
+                    ))}                
+                </ul>
+
+                <div className="flex justify-between mt-10 ml-10 mr-10">
+                <span className="font-bold text-xl">Total:</span>
+                <span className="text-xl font-semibold">
+                    Q{shoppingCart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}
+                </span>
+                </div>
+
+                <div className="modal-action">
+                    <button className='btn btn-ghost mr-5'>{user.address}</button>
+                    <form method="dialog" className='flex flex-row gap-5 justify-center align-middle items-center'>
+                    {/* if there is a button in form, it will close the modal */}
+                        <button className="btn">Close</button>
+                        <button disabled={shoppingCart.length === 0} className="btn btn-primary">Comprar</button>
+                    </form>
+                </div>
+            </div>
+        </dialog>
+        
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
@@ -72,7 +160,7 @@ export default function Dashboard ({ handleAuth }) {
                         alt="Banner" />
                 </div>
                 { isRestaurant && selectedRestaurant ? (
-                <div className='mt-[10%] w-full'>
+                    <div className='mt-[10%] w-full'>
                     <div className='fixed top-0 left-0 w-screen h-screen -z-10'>
                         <img src={selectedRestaurant.banner_img || "https://images.unsplash.com/photo-1543353071-10c8ba85a904?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D"}
                         className='w-full h-full object-cover'
@@ -101,6 +189,7 @@ export default function Dashboard ({ handleAuth }) {
                                 {selectedDishes.map((dish, index) => (
                                     <div key={index} className="w-full">
                                         <Dish_card
+                                            handleDish={handleDish}
                                             dish_img={dish.img}
                                             dish_name={dish.name}
                                             dish_rating={dish.rating}
@@ -131,7 +220,7 @@ export default function Dashboard ({ handleAuth }) {
                     <div className='mt-auto bg-base-100 ml-auto xl:h-[85%] w-auto sm:h-screen'>
                         <div className="carousel rounded-box ml-auto mr-auto">
                             {restaurants.map((restaurant, index) => (
-                            <div className="carousel-item" key={index}>
+                                <div className="carousel-item" key={index}>
                                 <div className="w-full">
                                 <Restaurant_card
                                     handleRestaurant={handleRestaurant}
--- src/components/dish_card.jsx
+++ src/components/dish_card.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 
-const Dish_card = ({ dish_img, dish_name, dish_rating, dish_price, dish_description }) => {
+const Dish_card = ({ handleDish, dish_img, dish_name, dish_rating, dish_price, dish_description }) => {
+  const modalId = `modal-${dish_name.replace(/\s+/g, '-')}`; // ejemplo: "Pizza Suprema" => "modal-Pizza-Suprema"
+
   return (
     <>
-        <div className="card max-w-100 min-h-80 max-h-80 m-5 bg-base-100 shadow-sm cursor-pointer hover:scale-103 transition-all" onClick={() => {alert(`${dish_name}`)}}>
-            <figure>
-                <img
-                src={dish_img}
-                width={"full"}
-                alt="Imagen del platillo" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title text-4xl">{dish_name}</h2>
-                <h2 className='card-title text-3xl font-extralight text-gray-500'>Q{dish_price}</h2>
-                <div className="card-actions justify-center">
-                    <span className="w-full btn btn-sm btn-accent text-3xl">+</span>
-                </div>
-            </div>
+      <dialog id={modalId} className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <img src={dish_img} alt="Imagen del platillo" className="w-full rounded-lg" />
+          <h3 className="font-bold text-3xl mt-4">{dish_name}</h3>
+          <p className="text-xl py-4">{dish_description}</p>
+          <h3 className='font-extralight text-3xl'>Q{dish_price}</h3>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Cerrar</button>
+              <button className="btn btn-primary ml-5" onClick={() => handleDish(dish_img, dish_name, dish_price, dish_description)}>Agregar</button>
+            </form>
+          </div>
         </div>
+      </dialog>
+
+      <div
+        className="card max-w-100 min-h-80 max-h-80 m-5 bg-base-100 shadow-sm cursor-pointer hover:scale-103 transition-all"
+        onClick={() => document.getElementById(modalId).showModal()}
+      >
+        <figure>
+          <img src={dish_img} width={"full"} alt="Imagen del platillo" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-4xl">{dish_name}</h2>
+          <h2 className="card-title text-3xl font-extralight text-gray-500">Q{dish_price}</h2>
+          <div className="card-actions justify-center">
+            <span className="w-full btn btn-sm btn-accent text-3xl">+</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
